a7756a7e523f91845b59a9674c2220d964a26c8c
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2022-09-26 22:38:48 +0530

    Update python gradesheet with login.py

--- python gradesheet with login.py
+++ python gradesheet with login.py
@@ -1,13 +1,17 @@
-from statistics import mean as m             #    login id: gaurav  password: 2020
-                                             # login id : adarsh  passwod : 2021
-admins={'gaurav':2020 , 'adarsh':2021}
-students_list={'gauarav':98,'adarsh':96,'dhanraj':95}
+from statistics import mean as m
+admn1=input('enter 1st username\n')
+pasw1=int(input('enter 1st integer password\n'))
+admn2=input('enter 2nd username\n')
+pasw2=int(input('enter 2nd  integer password\n'))
+admins={admn1:pasw1 , admn2:pasw2}
+
+students_list={'gaurav':[92],'adarsh':[96],'dhanraj':[95]}
 def entergrades():
      enter_students=input('students name:')
      enter_grade=int(input('grade:'))
      if enter_students in students_list:
          print('adding grade...')
-         students_list[enter_students].append(float(enter_grade))
+         students_list[enter_students].append(enter_grade)
      else:
          print('student does not exist')
      print(students_list)   
@@ -15,28 +19,25 @@ def entergrades():
 def removestudents():
      want_to_remove=input('enter students:')
      if want_to_remove in students_list:
-          print('removing students')
+          print('removing students...')
           del  students_list[want_to_remove]
           print(students_list)
      else:
           print('no name matched')
+
 def average():
      for eachstudent in students_list:
           gradelist=students_list(eachstudent)
           avg=m(gradelist)
           print(eachstudent,'average marks:',avg)
-    
-       
-    
-   
-          
-     
-     
 
-     
-         
-     
-     
+def addstudents():
+    newstd=input("enter name of student to add in list:")
+    newgrd=input('enter grade of new students:')
+    print("adding .....")
+    students_list[newstd]=list(map(int,(newgrd+' ').split()))
+    print(students_list)
+          
 def main():
 
     print("""welcome to grade list
@@ -44,9 +45,10 @@ def main():
     [1]-enter grades
     [2]-remove students
     [3]-students average grades
-    [4]-exit
+    [4]- add students
+    [5]-exit
     """)
-    option =input("what would you like to do today?(choose an option) ")
+    option =input("what would you like to do today?(choose an option)\n ")
     if option=='1':
         entergrades()
     elif option =='2':
@@ -54,12 +56,13 @@ def main():
     elif option=='3':
         average()
     elif option=='4':
-         exit()
+         addstudents()
+    elif option=='5':
+        exit()
     else:
           print('please enter a valid number , try again')
           
-     
-      
+          
 login=input('username: ')
 passw=int(input('password: '))
 if login in admins:
@@ -67,19 +70,11 @@ if login in admins:
         
           print("welcome, ",login)
           while True:
-             main()
-        
+             main()      
       
-       
-
-       
-         
-
     else:
         print('wrong password !, will detonate in 5 seconds')
          
-    
-    
 else:
     print('inavalid username !')
     
